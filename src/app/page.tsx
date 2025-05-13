522267cbc157887332f8534fd5b8bcc85368c027
Hussain Ali <hussainalee@icloud.com>
2025-05-13 10:43:19 +0500

    layout changes in main screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
+import { ForgotPassword } from "@/components/forgot-password";
 import { LoginForm } from "@/components/login-form";
 import { RegisterForm } from "@/components/register-form";
 import { use } from "react";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: SearchParams;
-}) {
+export default function Home({ searchParams }: { searchParams: SearchParams }) {
   const searchValue = use(searchParams);
-  const query = searchValue?.mode;
+  const query = searchValue?.mode || "signIn";
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
-        {query == "signIn" ? <LoginForm /> : <RegisterForm />}
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        {query == "signIn" && <LoginForm />}
+        {query == "signUp" && <RegisterForm />}
+        {query == "forgotPassword" && <ForgotPassword />}
       </div>
     </div>
   );
